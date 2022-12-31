700d69b362cf8441da83bf4d532dbf88ac965c89
jigargosar <jigar.gosar@gmail.com>
2022-12-31 14:40:04 +0530

    svg canvas

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,39 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (div, text)
+import Html.Attributes exposing (style)
+import Svg exposing (svg)
+import Svg.Attributes as S exposing (fill, stroke, viewBox)
 
 
 main =
-    text "hello world 555"
+    div
+        [ style "display" "grid"
+        , style "height" "100%"
+        , style "padding" "2rem"
+        , style "box-sizing" "border-box"
+        ]
+        [ Html.node "style"
+            []
+            [ text """
+                :root{height:100%;}
+                body{height:100%;}
+                """
+            ]
+        , svg
+            [ viewBox "0 0 500 500"
+            , style "display" "block"
+            , style "background-color" "black"
+            , style "max-width" "100%"
+            , style "max-height" "100%"
+            , style "place-self" "center"
+            , stroke "white"
+            , fill "transparent"
+            ]
+            [ Svg.g
+                [ style "transform" "translate(50%, 50%)"
+                ]
+                [ Svg.circle [ S.r "100" ] []
+                ]
+            ]
+        ]
